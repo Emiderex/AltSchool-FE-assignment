5f11b1c23cdc71cfeeef64f9cf0a1dfdab621db2
Emmanuel <emiderex112@gmail.com>
2025-09-14 20:48:33 +0100

    Challenge two done and tested successfully

--- src/assignment.js
+++ src/assignment.js
@@ -33,11 +33,18 @@ export function sumOfNumbersTo(destination) {
 export function evenNumbersWithin(destination) {
     console.log(destination)
     // get the number from 0 to destination
-    const sum = 0
-    const count = 0
+    let sum = 0
     const arrayOfEvenNumbers = []
 
     // write your code here
+    for (let i = 0; i <= destination; i++) {
+        if (i % 2 === 0) {
+            arrayOfEvenNumbers.push(i)
+            sum += i
+        }
+    }
+
+    const count = arrayOfEvenNumbers.length
 
     return {
         count,
